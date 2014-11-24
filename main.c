a43ad9b41b329b927b947e3dbfa5e3a4968fc7bb
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2014-11-24 17:48:16 -0500

    Added pipe support
    	* /dev/tty input
    	* stdin and all

--- main.c
+++ main.c
@@ -6,19 +6,25 @@ int
 main(int argc, char *argv[])
 {
 	FILE *fp;
+	FILE *usrin;
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
 	int i = 0;
 
-	if(argc < 2){
-		err(1, "need to specify a file\n");
+	if(argc == 1){
+		fp = stdin;
+	}else
+		fp = fopen(argv[1], "r");
+
+	if(fp == NULL){
+		err(1, "cannot open %s", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	fp = fopen(argv[1], "r");
-	if(fp == NULL){
-		err(1, "cannot open %s\n", argv[1]);
-		exit(EXIT_FAILURE);
+
+	usrin = fopen("/dev/tty", "r");
+	if(usrin == NULL){
+		err(1, "cannot open /dev/tty/");
 	}
 
 	while((read = getline(&line, &len, fp)) != -1){
@@ -26,7 +32,7 @@ main(int argc, char *argv[])
 		i++;
 		if(i > 20){
 			i = 0;
-			getchar();
+			fgetc(usrin);
 		}
 	}
 	free(line);
