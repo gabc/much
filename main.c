1f983e07eddb77cf05bb373000b095cd7c44187d
Gabriel Beauchamp <beauchampgabriel@gmail.com>
2014-11-30 20:23:32 -0500

    Clean up

--- main.c
+++ main.c
@@ -2,41 +2,64 @@
 #include <stdlib.h>
 #include <ncurses.h>
 
-int
-main(int argc, char *argv[])
+FILE *fp;
+FILE *usrin;
+char *line = NULL;
+size_t len = 0;
+ssize_t read;
+
+void
+getl(int n)
 {
-	FILE *fp;
-	FILE *usrin;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	int i = 0;
+	int i;
+	for(i = 0; i < n; i++){
+		if(getline(&line, &len, fp) == -1)
+			return;
+	}
+}
 
-	if(argc == 1){
-		fp = stdin;
-	}else
-		fp = fopen(argv[1], "r");
+void
+cleanup(void)
+{
+	free(line);
+	fclose(fp);
+	fclose(usrin);
+}
 
+void
+openfile(void)
+{
 	if(fp == NULL){
 		err(1, "cannot open %s", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
 	usrin = fopen("/dev/tty", "r");
-	if(usrin == NULL){
+	if(usrin == NULL)
 		err(1, "cannot open /dev/tty/");
-	}
+}
 
+int
+main(int argc, char *argv[])
+{
+	int i;
+
+	if(argc == 1)
+		fp = stdin;
+	else
+		fp = fopen(argv[1], "r");
+
+	openfile();
+	
+	i = 0;
 	while((read = getline(&line, &len, fp)) != -1){
-		printf("%s", line);
+		printw("%s", line);
 		i++;
 		if(i > 20){
 			i = 0;
 			fgetc(usrin);
 		}
 	}
-	free(line);
-	fclose(fp);
-	fclose(usrin);
+
+	cleanup();
 	exit(EXIT_SUCCESS);
 }
